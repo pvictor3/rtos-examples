404fcc3807a782120d5337b1697377382afe3f25
Victor Parra <pvictor3@gmail.com>
2019-05-07 22:40:51 -0500

    Creacion de proyecto en Keil uvision 5, compilacion exitosa

--- round_robin_scheduler/OS.c
+++ round_robin_scheduler/OS.c
@@ -100,3 +100,30 @@ void OS_launch(uint32_t timeSlice)
 }
 
 
+/*
+    * setInitialStack
+    * initialize the stack of the given thread
+    * input: number of thread
+    * output: none
+*/
+static void setInitialStack(int i)
+{
+	tcbs[i].sp = &stacks[i][STACKSIZE - 16];	//thread stack pointer
+	stacks[i][STACKSIZE - 1] = 0x01000000;	//Thumb bit
+	stacks[i][STACKSIZE - 3] = 0x14141414;	//R14
+	stacks[i][STACKSIZE - 4] = 0x12121212;	//R12
+	stacks[i][STACKSIZE - 5] = 0x03030303; // R3
+	stacks[i][STACKSIZE - 6] = 0x02020202; // R2
+	stacks[i][STACKSIZE - 7] = 0x01010101; // R1
+	stacks[i][STACKSIZE - 8] = 0x00000000; // R0
+	stacks[i][STACKSIZE - 9] = 0x11111111; // R11
+	stacks[i][STACKSIZE - 10] = 0x10101010; // R10
+	stacks[i][STACKSIZE - 11] = 0x09090909; // R9
+	stacks[i][STACKSIZE - 12] = 0x08080808; // R8
+	stacks[i][STACKSIZE - 13] = 0x07070707; // R7
+	stacks[i][STACKSIZE - 14] = 0x06060606; // R6
+	stacks[i][STACKSIZE - 15] = 0x05050505; // R5
+	stacks[i][STACKSIZE - 16] = 0x04040404;	//R4
+}
+
+
--- round_robin_scheduler/os_asm.s
+++ round_robin_scheduler/os_asm.s
@@ -1,17 +1,16 @@
-/*
-    * os_asm.s
-    * A very simple RTOS with round robin scheduler
-    * vmph
-    * 07/05/2019
-    * 
-*/
+
+    ; os_asm.s
+    ; A very simple RTOS with round robin scheduler
+    ; vmph
+    ; 07/05/2019
+    ; 
 
         AREA |.text|, CODE, READONLY, ALIGN=2
         THUMB
         REQUIRE8
         PRESERVE8
 
-        EXTERN  RunPt            ; currently running thread
+        EXTERN  ptRun            ; currently running thread
         EXPORT  OS_disableInterrupts
         EXPORT  OS_enableInterrupts
         EXPORT  startOS
@@ -31,7 +30,7 @@ OS_enableInterrupts
 SysTick_Handler                ; 1) Saves R0-R3,R12,LR,PC,PSR
     CPSID   I                  ; 2) Prevent interrupt during switch
     PUSH    {R4-R11}           ; 3) Save remaining regs r4-11
-    LDR     R0, =RunPt         ; 4) R0=pointer to RunPt, old thread
+    LDR     R0, =ptRun         ; 4) R0=pointer to RunPt, old thread
     LDR     R1, [R0]           ;    R1 = RunPt
     STR     SP, [R1]           ; 5) Save SP into TCB
     LDR     R1, [R1,#4]        ; 6) R1 = RunPt->next
@@ -42,7 +41,7 @@ SysTick_Handler                ; 1) Saves R0-R3,R12,LR,PC,PSR
     BX      LR                 ; 10) restore R0-R3,R12,LR,PC,PSR
 
 startOS
-    LDR     R0, =RunPt         ; currently running thread
+    LDR     R0, =ptRun         ; currently running thread
     LDR     R2, [R0]           ; R2 = value of RunPt
     LDR     SP, [R2]           ; new thread SP; SP = RunPt->stackPointer;
     POP     {R4-R11}           ; restore regs r4-11
